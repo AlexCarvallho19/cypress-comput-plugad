b293296e4c77151613e6e0370bef63ebddb97197
alexcarvallho19 <a.karrel2010@gmail.com>
2023-05-03 01:05:16 -0300

    :white_check_mark: Introduzindo fases 5,6,7 e 8

--- cypress/e2e/gui/fases/fases.cy.js
+++ cypress/e2e/gui/fases/fases.cy.js
@@ -1339,5 +1339,613 @@ describe('Scripts Fase 4', () => {
 
 
   })
+  it('CT064 - Respondendo segunda atividade da fase 4 corretamente', () => {
+    cy.contains('FASE 4').click()
+    cy.get('.bi.bi-chevron-right').click()
+    cy.get('.bi.bi-chevron-right').click()
+    cy.url().should('include', '/fase-4-3')
 
+    //Escrevendo texto e clicando em enviar
+
+    cy.get('#answer').type('1')
+    cy.get('button[type="button"]').click()
+    
+    cy.wait(1000)
+
+    cy.get('#answer').type('10')
+    cy.get('button[type="button"]').click()
+  })
+  it('CT065 - Respondendo todas atividades da fase 4 corretamente', () => {
+    cy.contains('FASE 4').click()
+    cy.get('.bi.bi-chevron-right').click()
+    cy.get('.bi.bi-chevron-right').click()
+    cy.url().should('include', '/fase-4-3')
+
+    //Escrevendo texto e clicando em enviar
+
+    cy.get('#answer').type('1')
+    cy.get('button[type="button"]').click()
+    
+    cy.wait(1000)
+
+    cy.get('#answer').type('10')
+    cy.get('button[type="button"]').click()
+
+    cy.wait(1000)
+
+    cy.get('#answer').type('21')
+    cy.get('button[type="button"]').click()
+    
+    cy.wait(1000)
+
+    cy.get('#answer').type('4')
+    cy.get('button[type="button"]').click()
+    cy.wait(1000)
+
+    cy.get('#answer').type('5')
+    cy.get('button[type="button"]').click()
+    cy.wait(1000)
+
+    cy.get('#answer').type('5')
+    cy.get('button[type="button"]').click()
+    
+    cy.wait(1000)
+
+    cy.get('#answer').type('19')
+    cy.get('button[type="button"]').click()
+    cy.wait(1000)
+
+    cy.get('#answer').type('20')
+    cy.get('button[type="button"]').click()
+    cy.wait(1000)
+
+    cy.get('#answer').type('15')
+    cy.get('button[type="button"]').click()
+    cy.wait(1000)
+
+    cy.get('#answer').type('21')
+    cy.get('button[type="button"]').click()
+    cy.wait(1000)
+
+    cy.get('#answer').type('16')
+    cy.get('button[type="button"]').click()
+    cy.wait(1000)
+
+    cy.get('#answer').type('18')
+    cy.get('button[type="button"]').click()
+    cy.wait(1000)
+
+    cy.get('#answer').type('5')
+    cy.get('button[type="button"]').click()
+    cy.wait(1000)
+
+    cy.get('#answer').type('19')
+    cy.get('button[type="button"]').click()
+    cy.wait(1000)
+
+    cy.get('#answer').type('15')
+    cy.get('button[type="button"]').click()
+  })
+
+  it('CT066 - Respondendo ultima atividade da fase 4 c/ resposta Correta mas sem espaçamento', () => {
+    cy.contains('FASE 4').click()
+    cy.get('.bi.bi-chevron-right').click()
+    cy.get('.bi.bi-chevron-right').click()
+    cy.url().should('include', '/fase-4-3')
+
+    //Escrevendo texto e clicando em enviar
+
+    cy.get('#answer').type('1')
+    cy.get('button[type="button"]').click()
+    
+    cy.wait(1000)
+
+    cy.get('#answer').type('10')
+    cy.get('button[type="button"]').click()
+
+    cy.wait(1000)
+
+    cy.get('#answer').type('21')
+    cy.get('button[type="button"]').click()
+    
+    cy.wait(1000)
+
+    cy.get('#answer').type('4')
+    cy.get('button[type="button"]').click()
+    cy.wait(1000)
+
+    cy.get('#answer').type('5')
+    cy.get('button[type="button"]').click()
+    cy.wait(1000)
+
+    cy.get('#answer').type('5')
+    cy.get('button[type="button"]').click()
+    
+    cy.wait(1000)
+
+    cy.get('#answer').type('19')
+    cy.get('button[type="button"]').click()
+    cy.wait(1000)
+
+    cy.get('#answer').type('20')
+    cy.get('button[type="button"]').click()
+    cy.wait(1000)
+
+    cy.get('#answer').type('15')
+    cy.get('button[type="button"]').click()
+    cy.wait(1000)
+
+    cy.get('#answer').type('21')
+    cy.get('button[type="button"]').click()
+    cy.wait(1000)
+
+    cy.get('#answer').type('16')
+    cy.get('button[type="button"]').click()
+    cy.wait(1000)
+
+    cy.get('#answer').type('18')
+    cy.get('button[type="button"]').click()
+    cy.wait(1000)
+
+    cy.get('#answer').type('5')
+    cy.get('button[type="button"]').click()
+    cy.wait(1000)
+
+    cy.get('#answer').type('19')
+    cy.get('button[type="button"]').click()
+    cy.wait(1000)
+
+    cy.get('#answer').type('15')
+    cy.get('button[type="button"]').click()
+    //Inserindo resposta CORRETA mas sem espaçamento
+    cy.wait(1000)
+
+    cy.get('#answer').type('ajude estoupreso')
+    cy.get('button[type="button"]').click()
+    cy.contains('Tente outra vez.').should('be.visible')
+  })
+})
+
+  // --------------------- Fase 5 --------------------------
+
+describe('Scripts Fase 5', () => {
+    beforeEach(() => {
+      cy.visit('https://wyllianyurk77.github.io/plugged-computing-web-front/')
+      cy.gui_iniciar()
+    })
+
+    it('CT067 - Mostrar fase 5', () => {
+      cy.contains('FASE 5').click()
+      cy.url().should('include', '/fase-5-1')
+    })
+
+    it('CT068 - Navegar até segunda descrição da fase 5', () => {
+      cy.contains('FASE 5').click()
+      cy.get('.bi.bi-chevron-right').click()
+      cy.url().should('include', '/fase-5-2')
+    })
+    
+    it('CT069 - Navegar até a primeira atividade fase 5', () => {
+      cy.contains('FASE 5').click()
+      cy.get('.bi.bi-chevron-right').click()
+      cy.get('.bi.bi-chevron-right').click()
+    })
+    
+    it('CT070 - Respondendo primeira atividade fase 5 em branco', () => {
+      cy.contains('FASE 5').click()
+      cy.get('.bi.bi-chevron-right').click()
+      cy.get('.bi.bi-chevron-right').click()
+
+      //cy.get('#answer').type('')
+      cy.get('button[type="button"]').click()
+      cy.contains('Este campo não pode ficar em branco.').should('be.visible')
+    })
+    it('CT071 - Respondendo segunda atividade fase 5 sem espaçamento', () => {
+      cy.contains('FASE 5').click()
+      cy.get('.bi.bi-chevron-right').click()
+      cy.get('.bi.bi-chevron-right').click()
+
+      
+      cy.get('#answer').type('alex')
+      cy.get('button[type="button"]').click()
+
+      cy.wait(1000)
+      cy.get('#answer').type('muito prazeralex')
+      cy.get('button[type="button"]').click()
+      cy.contains('Tente outra vez.').should('be.visible')
+    })
+    it('CT072 - Respondendo segunda atividade fase 5 com espaçamento ao final', () => {
+      cy.contains('FASE 5').click()
+      cy.get('.bi.bi-chevron-right').click()
+      cy.get('.bi.bi-chevron-right').click()
+
+      //Passando resposta para atividade seguinte com um "espaço" ao final do nome
+      cy.get('#answer').type('alex ')
+      cy.get('button[type="button"]').click()
+
+      //Verificando se ele aceita a resposta sem o espaço depois de "alex"
+      cy.wait(1000)
+      cy.get('#answer').type('muito prazer alex')
+      cy.get('button[type="button"]').click()
+      cy.contains('Tente outra vez.').should('be.visible')
+    })
+})
+
+      // ------------------------------------------ Fase 6 -----------------------------------------
+
+  describe('Scripts Fase 6', () => {
+    beforeEach(() => {
+      cy.visit('https://wyllianyurk77.github.io/plugged-computing-web-front/')
+      cy.gui_iniciar()
+    })
+
+    it('CT073 - Mostrar fase 6', () => {
+      cy.contains('FASE 6').click()
+      cy.url().should('include', '/fase-6-1')
+    })
+
+    it('CT074 - Navegar até segunda descrição da fase 6', () => {
+      cy.contains('FASE 6').click()
+      cy.get('.bi.bi-chevron-right').click()
+      cy.url().should('include', '/fase-6-2')
+    })
+    
+    it('CT075 - Navegar até a primeira atividade fase 6', () => {
+      cy.contains('FASE 6').click()
+      cy.get('.bi.bi-chevron-right').click()
+      cy.get('.bi.bi-chevron-right').click()
+    })
+    
+    it('CT076 - Respondendo primeira atividade fase 6 corretamente', () => {
+      cy.contains('FASE 6').click()
+      cy.get('.bi.bi-chevron-right').click()
+      cy.get('.bi.bi-chevron-right').click()
+
+      //Clicando na alternativa correta
+      cy.contains('11110').click()
+      cy.url().should('include', '/fase-6-3')
+    })
+
+    it('CT077 - Respondendo primeira atividade fase 6 incorretamente', () => {
+      cy.contains('FASE 6').click()
+      cy.get('.bi.bi-chevron-right').click()
+      cy.get('.bi.bi-chevron-right').click()
+
+      //Clicando na alternativa correta
+      cy.contains('01111').click()
+      cy.contains('Tente outra vez').should('be.visible')
+    })
+
+    it('CT078 - Respondendo segunda atividade fase 6 corretamente', () => {
+      cy.contains('FASE 6').click()
+      cy.get('.bi.bi-chevron-right').click()
+      cy.get('.bi.bi-chevron-right').click()
+
+      cy.contains('11110').click()
+
+      //Clicando na alternativa correta
+      cy.contains('691').click()
+      cy.url().should('include', '/fase-6-3')
+    })
+
+    it('CT079 - Respondendo segunda atividade fase 6 incorretamente', () => {
+      cy.contains('FASE 6').click()
+      cy.get('.bi.bi-chevron-right').click()
+      cy.get('.bi.bi-chevron-right').click()
+
+      cy.contains('11110').click()
+
+      //Clicando na alternativa correta
+      cy.contains('394').click()
+      cy.contains('Tente outra vez').should('be.visible')
+    })
+
+    it('CT080 - Respondendo terceira atividade fase 6 corretamente', () => {
+      cy.contains('FASE 6').click()
+      cy.get('.bi.bi-chevron-right').click()
+      cy.get('.bi.bi-chevron-right').click()
+
+      //Clicando na alternativa correta
+      cy.contains('11110').click()
+      //Clicando na alternativa correta
+      cy.contains('691').click()
+      
+      cy.contains('1.048.575').click()
+      cy.url().should('include', '/fase-6-4')
+    })
+
+    it('CT081 - Respondendo terceira atividade fase 6 incorretamente', () => {
+      cy.contains('FASE 6').click()
+      cy.get('.bi.bi-chevron-right').click()
+      cy.get('.bi.bi-chevron-right').click()
+
+      //Clicando na alternativa correta
+      cy.contains('11110').click()
+      //Clicando na alternativa correta
+      cy.contains('691').click()
+      
+      cy.contains('1.073.741').click()
+      cy.contains('Tente outra vez').should('be.visible')
+    })
+
+    it('CT082 - Clicando no botão “↺” da tela de parabéns na Fase 6', () => {
+      cy.contains('FASE 6').click()
+      cy.get('.bi.bi-chevron-right').click()
+      cy.get('.bi.bi-chevron-right').click()
+
+      //Clicando na alternativa correta
+      cy.contains('11110').click()
+      //Clicando na alternativa correta
+      cy.contains('691').click()
+      
+      cy.contains('1.048.575').click()
+      cy.url().should('include', '/fase-6-4')
+
+      //Clicando no botão “↺” da Fase 3
+      cy.get('.bi.bi-arrow-counterclockwise').click()
+      cy.url().should('include', '/fase-6-1')
+    })
+
+    it('CT083 - Clicando no botão “>” da tela de parabéns na Fase 6', () => {
+      cy.contains('FASE 6').click()
+      cy.get('.bi.bi-chevron-right').click()
+      cy.get('.bi.bi-chevron-right').click()
+
+      //Clicando na alternativa correta
+      cy.contains('11110').click()
+      //Clicando na alternativa correta
+      cy.contains('691').click()
+      
+      cy.contains('1.048.575').click()
+      cy.url().should('include', '/fase-6-4')
+
+      //Clicando no botão ">"
+      cy.get('.bi.bi-chevron-right').click()
+      cy.url().should('include', '/fases')
+    })
+})
+
+    // ------------------------------------------ Fase 7 -----------------------------------------
+
+  describe('Scripts Fase 7', () => {
+    beforeEach(() => {
+      cy.visit('https://wyllianyurk77.github.io/plugged-computing-web-front/')
+      cy.gui_iniciar()
+    })
+
+    it('CT084 - Mostrar fase 7', () => {
+      cy.contains('FASE 7').click()
+      cy.url().should('include', '/fase-7-1')
+    })
+
+    it('CT085 - Navegar até primeira atividade da fase 7', () => {
+      cy.contains('FASE 7').click()
+      cy.get('.bi.bi-chevron-right').click()
+      cy.url().should('include', '/fase-7-2')
+    })
+
+    it('CT086 - Respondendo primeira atividade fase 7 corretamente', () => {
+      cy.contains('FASE 7').click()
+      cy.get('.bi.bi-chevron-right').click()
+
+      //Clicando na alternativa correta
+      cy.contains('O número é dobrado').click()
+      cy.url().should('include', '/fase-7-2')
+    })
+    
+    it('CT087 - Respondendo primeira atividade fase 7 incorretamente', () => {
+      cy.contains('FASE 7').click()
+      cy.get('.bi.bi-chevron-right').click()
+
+      //Clicando na alternativa correta
+      cy.contains('É dividido por 2').click()
+      cy.contains('Tente outra vez').should('be.visible')
+    })
+
+    it('CT088 - Respondendo segunda atividade fase 7 corretamente', () => {
+      cy.contains('FASE 7').click()
+      cy.get('.bi.bi-chevron-right').click()
+
+      //Clicando na alternativa correta
+      cy.contains('O número é dobrado').click()
+
+      //Clicando na alternativa correta
+      cy.contains('18').click()
+      cy.url().should('include', '/fase-7-2')
+    })
+
+    it('CT089 - Respondendo segunda atividade fase 7 incorretamente', () => {
+      cy.contains('FASE 7').click()
+      cy.get('.bi.bi-chevron-right').click()
+
+      //Clicando na alternativa correta da primeira atividade
+      cy.contains('O número é dobrado').click()
+
+      //Clicando na alternativa incorreta
+      cy.contains('9').click()
+      cy.contains('Tente outra vez').should('be.visible')
+    })
+
+    it('CT090 - Respondendo terceira atividade fase 7 corretamente', () => {
+      cy.contains('FASE 7').click()
+      cy.get('.bi.bi-chevron-right').click()
+
+      //Clicando na alternativa correta
+      cy.contains('O número é dobrado').click()
+
+      //Clicando na alternativa correta
+      cy.contains('18').click()
+      
+      //Clicando na alternativa correta terceira atv
+      cy.contains('128').click()
+      cy.url().should('include', '/fase-7-3')
+    })
+    
+    it('CT091 - Respondendo terceira atividade fase 7 incorretamente', () => {
+      cy.contains('FASE 7').click()
+      cy.get('.bi.bi-chevron-right').click()
+
+      //Clicando na alternativa correta
+      cy.contains('O número é dobrado').click()
+
+      //Clicando na alternativa correta
+      cy.contains('18').click()
+      
+      //Clicando na alternativa incorreta terceira atv
+      cy.contains('256').click()
+      cy.contains('Tente outra vez').should('be.visible')
+    })
+
+    it('CT092 - Clicando no botão “↺” da tela de parabéns na Fase 7', () => {
+      cy.contains('FASE 7').click()
+      cy.get('.bi.bi-chevron-right').click()
+
+      //Clicando na alternativa correta
+      cy.contains('O número é dobrado').click()
+
+      //Clicando na alternativa correta
+      cy.contains('18').click()
+      
+      //Clicando na alternativa correta terceira atv
+      cy.contains('128').click()
+
+      //Clicando no botão “↺” da Fase 3
+      cy.get('.bi.bi-arrow-counterclockwise').click()
+      cy.url().should('include', '/fase-7-1')
+    })
+
+    it('CT093 - Clicando no botão “>” da tela de parabéns na Fase 7', () => {
+      cy.contains('FASE 7').click()
+      cy.get('.bi.bi-chevron-right').click()
+
+      //Clicando na alternativa correta
+      cy.contains('O número é dobrado').click()
+
+      //Clicando na alternativa correta
+      cy.contains('18').click()
+      
+      //Clicando na alternativa correta terceira atv
+      cy.contains('128').click()
+
+      //Clicando no botão ">"
+      cy.get('.bi.bi-chevron-right').click()
+      cy.url().should('include', '/fases')
+    })
+})
+
+        // ------------------------------------------ Fase 8 -----------------------------------------
+
+  describe('Scripts Fase 8', () => {
+    beforeEach(() => {
+      cy.visit('https://wyllianyurk77.github.io/plugged-computing-web-front/')
+      cy.gui_iniciar()
+    })
+
+    it('CT094 - Mostrar fase 8', () => {
+      cy.contains('FASE 8').click()
+      cy.url().should('include', '/fase-8-1')
+    })
+
+    it('CT095 - Navegar entre todas abas da fase 8', () => {
+      cy.contains('FASE 8').click()
+      cy.get('.bi.bi-chevron-right').click()
+      cy.url().should('include', '/fase-8-2')
+      cy.get('.bi.bi-chevron-right').click()
+      cy.url().should('include', '/fase-8-3')
+      cy.get('.bi.bi-chevron-right').click()
+      cy.url().should('include', '/fase-8-4')
+      cy.get('.bi.bi-chevron-right').click()
+      cy.url().should('include', '/fase-8-5')
+      cy.get('.bi.bi-chevron-right').click()
+      cy.url().should('include', '/fase-8-6')
+      cy.get('.bi.bi-chevron-right').click()
+      cy.url().should('include', '/fase-8-7')
+      cy.get('.bi.bi-chevron-right').click()
+      cy.url().should('include', '/fase-8-8')
+    })
+    
+    it('CT096 - Clicar no link "Formulário de avaliação" da fase 8', () => {
+      cy.contains('FASE 8').click()
+      cy.get('.bi.bi-chevron-right').click()
+      
+      cy.get('.bi.bi-chevron-right').click()
+      
+      cy.get('.bi.bi-chevron-right').click()
+      
+      cy.get('.bi.bi-chevron-right').click()
+     
+      cy.get('.bi.bi-chevron-right').click()
+      
+      cy.get('.bi.bi-chevron-right').click()
+      
+      cy.get('.bi.bi-chevron-right').click()
+
+      cy.contains('Formulário de avaliação').click()
+    })
+    it('CT097 - Clicar em Finalizar da fase 8', () => {
+      cy.contains('FASE 8').click()
+      cy.get('.bi.bi-chevron-right').click()
+      
+      cy.get('.bi.bi-chevron-right').click()
+      
+      cy.get('.bi.bi-chevron-right').click()
+      
+      cy.get('.bi.bi-chevron-right').click()
+     
+      cy.get('.bi.bi-chevron-right').click()
+      
+      cy.get('.bi.bi-chevron-right').click()
+      
+      cy.get('.bi.bi-chevron-right').click()
+
+      cy.contains('Finalizar').click()
+      cy.url().should('include', '/fase-8-9')
+    })
+
+    it('CT098 - Clicar no botão ">" da fase 8', () => {
+      cy.contains('FASE 8').click()
+      cy.get('.bi.bi-chevron-right').click()
+      
+      cy.get('.bi.bi-chevron-right').click()
+      
+      cy.get('.bi.bi-chevron-right').click()
+      
+      cy.get('.bi.bi-chevron-right').click()
+     
+      cy.get('.bi.bi-chevron-right').click()
+      
+      cy.get('.bi.bi-chevron-right').click()
+      
+      cy.get('.bi.bi-chevron-right').click()
+
+      //clicando em finalizar para ir até o botão do teste
+      cy.contains('Finalizar').click()
+
+      cy.get('.bi.bi-chevron-right').click()
+      cy.url().should('include', '/fases')
+    })
+    it('CT099 - Clicar no botão “↺” da fase 8', () => {
+      cy.contains('FASE 8').click()
+      cy.get('.bi.bi-chevron-right').click()
+      
+      cy.get('.bi.bi-chevron-right').click()
+      
+      cy.get('.bi.bi-chevron-right').click()
+      
+      cy.get('.bi.bi-chevron-right').click()
+     
+      cy.get('.bi.bi-chevron-right').click()
+      
+      cy.get('.bi.bi-chevron-right').click()
+      
+      cy.get('.bi.bi-chevron-right').click()
+
+      cy.contains('Finalizar').click()
+
+      //Clicando no botão “↺” da Fase 8
+      cy.get('.bi.bi-arrow-counterclockwise').click()
+      cy.url().should('include', '/fase-8-1')
+    })
+
+    
 })
